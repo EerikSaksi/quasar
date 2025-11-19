31f055199d38bc4144f243a31215977f500659d0
Eerik Saksi <saksi.eerik@gmail.com>
2025-11-19 13:59:54 +0000

    Initial commit, basic setup

---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <div class="text-h6">{{ title }}</div>
+      <div>{{ desciption }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  desciption: string;
+}>();
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,20 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <q-list class="q-pa-lg">
+    <q-list-item v-for="todo in todos" :key="todo.title + todo.description" class="q-ma-md">
+      <TodoComponent title="example" desciption="Body of the todo"></TodoComponent>
+    </q-list-item>
+  </q-list>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
+import TodoComponent from 'components/TodoComponent.vue';
+interface Todo {
+  title: string;
+  description: string;
 }
+const todos = ref<Todo[]>([
+  { title: 'First Todo', description: 'This is the first todo item.' },
+  { title: 'Second Todo', description: 'This is the second todo item.' },
+]);
 </script>

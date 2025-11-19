6541e0bc4ca9545d382806fe01545fe88c62c788
Eerik Saksi <saksi.eerik@gmail.com>
2025-11-19 15:41:55 +0000

    Editing and the like

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,8 +1,14 @@
 <template>
-  <q-card>
-    <q-card-section>
-      <div class="text-h6">{{ title }}</div>
-      <div>{{ desciption }}</div>
+  <q-card class="q-pa-md">
+    <q-card-section class="text-h6 flex col justify-between items-start q-pa-xs">
+      <p>{{ title }}</p>
+    </q-card-section>
+    <q-card-section class="q-pa-xs">
+      <p>{{ desciption }}</p>
+    </q-card-section>
+    <q-card-section class="row justify-between items-start q-pa-xs">
+      <q-btn @click="deleteTodo" round icon="close" padding="xs" />
+      <q-btn @click="deleteTodo" round icon="check" padding="xs" />
     </q-card-section>
   </q-card>
 </template>
@@ -11,5 +17,6 @@
 defineProps<{
   title: string;
   desciption: string;
+  deleteTodo: () => void;
 }>();
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,32 @@
 <template>
-  <q-list class="q-pa-lg">
-    <q-list-item v-for="todo in todos" :key="todo.title + todo.description" class="q-ma-md">
-      <TodoComponent title="example" desciption="Body of the todo"></TodoComponent>
-    </q-list-item>
-  </q-list>
+  <q-layout class="flex flex-center" style="height: 100vh">
+    <q-list class="q-pa-lg">
+      <q-list-item class="flex row items-center">
+        <q-input :borderless="false" v-model="newTodo" label="New Todo" class="q-pa-md" />
+        <q-btn
+          class="q-pa-md"
+          round
+          icon="add"
+          padding="xs"
+          @click="
+            todos.push({ title: newTodo, description: 'No description' });
+            newTodo = '';
+          "
+        />
+      </q-list-item>
+      <q-list-item v-for="(todo, i) in todos" :key="todo.title + todo.description" class="q-ma-md">
+        <TodoComponent
+          :title="todo.title"
+          :desciption="todo.description"
+          :delete-todo="
+            () => {
+              todos = todos.filter((_, index) => index !== i);
+            }
+          "
+        />
+      </q-list-item>
+    </q-list>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
@@ -17,4 +40,6 @@ const todos = ref<Todo[]>([
   { title: 'First Todo', description: 'This is the first todo item.' },
   { title: 'Second Todo', description: 'This is the second todo item.' },
 ]);
+
+const newTodo = ref('');
 </script>
